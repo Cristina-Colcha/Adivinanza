435a14b8568db779ed160b7a6f380ed91b93b91d
Cristina-Colcha <criscolcha@gmail.com>
2024-11-16 22:07:09 -0500

    feat: :art: cambio puerto

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const express = require('express');  // Importa el módulo Express
-const path = require('path');       // Módulo nativo de Node.js para gestionar rutas de archivos
-const app = express();              // Crea una instancia de la aplicación Express
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// Configura Express para servir archivos estáticos (como HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Establece el puerto donde el servidor escuchará las solicitudes
-const puerto = 3000;
+const puerto = process.env.PORT || 3000;
 
-// Inicia el servidor y escucha en el puerto definido
 app.listen(puerto, () => {
     console.log(`Servidor corriendo en http://localhost:${puerto}`);
 });
